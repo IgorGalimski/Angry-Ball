2d08ac54a0ac5e3f9c0cf586339443dc0bded596
IgorGalimski <igorg@fm-games.com>
2021-12-19 15:13:08 +0200

    Continue business logic

--- AngryBall/ViewController.swift
+++ AngryBall/ViewController.swift
@@ -6,15 +6,34 @@
 //
 
 import UIKit
+import SpriteKit
+import GameplayKit
 
-class ViewController: UIViewController {
-    
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let view = self.view as! SKView?
+        {
+            guard let spriteKitScene = GameScene(fileNamed: "GameScene") else { fatalError() }
+            
+            spriteKitScene.scaleMode = .aspectFill
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = false
+            view.showsNodeCount = false
+            
+            view.presentScene(spriteKitScene)
+        }
     }
 
+    override var prefersStatusBarHidden: Bool
+    {
+        return true
+    }
 
 }
 
