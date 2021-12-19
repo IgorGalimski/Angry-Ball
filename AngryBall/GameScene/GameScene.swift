34d69ac0028271f8b0a8cbb55077fd669ad72766
IgorGalimski <igorg@fm-games.com>
2021-12-19 15:22:41 +0200

    Finalize

--- AngryBall/GameScene/GameScene.swift
+++ AngryBall/GameScene/GameScene.swift
@@ -1,62 +1,56 @@
 //
-//  GameScene.swift
+//  AngryBallScene.swift
 //  AngryBall
-//
-//  Created by IgorGalimski on 19/12/2021.
-//
 
-import Foundation
+
 import SpriteKit
 import GameplayKit
 
-class GameScene : SKScene
-{
+
+class GameScene: SKScene {
+    
     private var ball = SKSpriteNode()
     
     private var originalBallPosition = CGPoint()
     
-    private var wasBallShoot: Bool = false
+    private var ballWasShot: Bool = false
     
     private var sceneBoxes = [SKSpriteNode]()
     
     private var boxesStartingPosition = [CGPoint]()
     
-    override func didMove(to view: SKView)
-    {
-        
-        guard let angryBall = childNode(withName: "ball") as? SKSpriteNode else { return }
+    override func didMove(to view: SKView) {
+        guard let angryBall = self.childNode(withName: "ball") as? SKSpriteNode  else { return }
         ball = angryBall
         originalBallPosition = ball.position
         
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        
-        for boxNumber in 1...8
-        {
-            guard let box = childNode(withName: "box\(boxNumber)") as? SKSpriteNode else { continue }
-            
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+    
+        for boxNumber in 1...8 {
+            guard let box = self.childNode(withName: "box\(boxNumber)") as? SKSpriteNode else { continue }
+
             sceneBoxes.append(box)
             boxesStartingPosition.append(box.position)
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        if !wasBallShoot
-        {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !ballWasShot {
             guard let touch = touches.first else { return }
-            
+
             let touchLocation = touch.location(in: self)
+
+            let touchedNodes = nodes(at: touchLocation)
             
-            let touchNodes = nodes(at: touchLocation)
-            
-            if !touchNodes.isEmpty
-            {
-                for node in touchNodes
-                {
+
+            if !touchedNodes.isEmpty {
+
+                for node in touchedNodes {
+
                     guard let spriteNode = node as? SKSpriteNode else { return }
-                    
-                    if spriteNode == ball
-                    {
+
+                    if spriteNode == ball {
+
                         ball.position = touchLocation
                     }
                 }
@@ -64,30 +58,30 @@ class GameScene : SKScene
         }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        if !wasBallShoot
-        {
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        if !ballWasShot {
+
             guard let touch = touches.first else { return }
-            
+
             let touchLocation = touch.location(in: self)
-            
-            let touchNodes = nodes(at: touchLocation)
-            
-            if !touchNodes.isEmpty
-            {
-                for node in touchNodes
-                {
+
+            let touchedNodes = nodes(at: touchLocation)
+ 
+            if !touchedNodes.isEmpty {
+
+                for node in touchedNodes {
+
                     guard let spriteNode = node as? SKSpriteNode else { return }
-                    
-                    if spriteNode == ball
-                    {
-                        if ball.position.x > 0.0
-                        {
+
+                    if spriteNode == ball {
+
+                        if ball.position.x > 0.0 {
+
                             ball.position = originalBallPosition
-                        }
-                        else
-                        {
+                        } else {
+
                             ball.position = touchLocation
                         }
                     }
@@ -96,66 +90,86 @@ class GameScene : SKScene
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        guard let touch = touches.first else { return }
-        
-        let touchLocation = touch.location(in: self)
-        
-        let touchNodes = nodes(at: touchLocation)
-        
-        if !touchNodes.isEmpty
-        {
-            for node in touchNodes
-            {
-                guard let spriteNode = node as? SKSpriteNode else { return }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+            if !ballWasShot {
+ 
+                guard let touch = touches.first else { return }
+ 
+                let touchLocation = touch.location(in: self)
+
+                let touchedNodes = nodes(at: touchLocation)
                 
-                if spriteNode == ball
-                {
-                    let ballX = -(touchLocation.x - originalBallPosition.x)
-                    let ballY = -(touchLocation.y - originalBallPosition.y)
-                    
-                    let impulse = CGVector(dx: ballX, dy: ballY)
-                    
-                    ball.physicsBody?.applyImpulse(impulse)
-                    ball.physicsBody?.applyAngularImpulse(-0.01)
-                    ball.physicsBody?.affectedByGravity = true
-                    
-                    wasBallShoot = true
+
+                if !touchedNodes.isEmpty {
+     
+                    for node in touchedNodes {
+
+                        guard let spriteNode = node as? SKSpriteNode else { return }
+     
+                        if spriteNode == ball {
+                            
+
+                            let ballX = -(touchLocation.x - originalBallPosition.x)
+                            let ballY = -(touchLocation.y - originalBallPosition.y)
+                            
+                     
+                            let impulse = CGVector(dx: ballX, dy: ballY)
+                            
+          
+                            ball.physicsBody?.applyImpulse(impulse)
+                            ball.physicsBody?.applyAngularImpulse(-0.01)
+                            ball.physicsBody?.affectedByGravity = true
+                            
+
+                            ballWasShot = true
+                            
+                        }
+                    }
                 }
             }
-        }
     }
     
-    override func update(_ currentTime: TimeInterval)
-    {
-        if let angryBallPhysicsBody = ball.physicsBody
+
+    override func update(_ currentTime: TimeInterval) {
+
+        if let angryBallPhysicsBoody = ball.physicsBody
         {
-            if angryBallPhysicsBody.velocity.dx <= 0.1 &&
-                angryBallPhysicsBody.velocity.dy <= 0.1 &&
-                angryBallPhysicsBody.angularVelocity <= 0.1 &&
-                wasBallShoot
-            {
+
+            if angryBallPhysicsBoody.velocity.dx <= 0.1 && angryBallPhysicsBoody.velocity.dy <= 0.1 && angryBallPhysicsBoody.angularVelocity <= 0.1 && ballWasShot {
+                
+           
                 ball.physicsBody?.affectedByGravity = false
+            
+                ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                 
-                ball.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
-                
+           
                 ball.physicsBody?.angularVelocity = .zero
                 
-                ball.zRotation = .zero
+  
+                ball.zRotation = 0.0
                 
+           
                 ball.position = originalBallPosition
                 
-                wasBallShoot = false
+        
+                ballWasShot = false
                 
-                for boxIndex in 0...7
-                {
+                for boxIndex in 0...7 {
+                    
                     let originalBoxPosition = boxesStartingPosition[boxIndex]
+                    
                     let box = sceneBoxes[boxIndex]
+              
                     box.position = originalBoxPosition
-                    box.zRotation = .zero
+                  
+                    box.zRotation = 0.0
+                 
                     box.physicsBody?.velocity = CGVector.zero
-                    box.physicsBody?.angularVelocity = .zero
+              
+                    box.physicsBody?.angularVelocity = 0.0
+                    
                 }
             }
         }
--- AngryBall/ViewController.swift
+++ AngryBall/ViewController.swift
@@ -7,33 +7,35 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+import GameKit
 
-class ViewController: UIViewController
-{
-    override func viewDidLoad()
-    {
+class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        if let view = self.view as! SKView?
-        {
-            guard let spriteKitScene = GameScene(fileNamed: "GameScene") else { fatalError() }
-            
+        if let view = self.view as! SKView?  {
+
+            guard let spriteKitScene = GameScene(fileNamed: "GameScene") else { fatalError()}
+
             spriteKitScene.scaleMode = .aspectFill
-            
+
             view.ignoresSiblingOrder = true
+
             view.showsFPS = false
+
             view.showsNodeCount = false
             
             view.presentScene(spriteKitScene)
+ 
         }
     }
-
-    override var prefersStatusBarHidden: Bool
-    {
-        return true
-    }
+    
+    
 
 }
 
